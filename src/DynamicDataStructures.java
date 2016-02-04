9fbe9cb76bcf43681ff5dfc18d8d77f0ac9f9230
drtonton <tkfoster26@gmail.com>
2016-02-04 11:19:55 -0500

    created DynamicDataStructures class (to demonstrate HashMaps, ArrayLists, and dynamic data structures

---
+++ src/DynamicDataStructures.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by noellemachin on 2/4/16.
+ */
+public class DynamicDataStructures {
+    public static void main(String[] args) {
+        Person alice = new Person("Alice booiuhkj", 25);
+        Person bob = new Person("Bob", 30);
+        Person charlie = new Person("Charlie", 40);
+// <Person> tells java to only accept Person objects in this array list
+        ArrayList <Person> people = new ArrayList<>();
+        people.add(alice);
+        people.add(bob);
+        people.add(charlie);
+
+        for (Person p : people) {
+            System.out.println(p.getName());
+        }
+
+/*          this is the alternative for loop, does the same function as above
+        for (int i = 0; i < people.size(); i++) {
+            System.out.println(people.get(i).getName());
+        } */
+
+        HashMap <String, Person> peopleMap = new HashMap<>();
+        peopleMap.put("Alice", alice);
+        peopleMap.put("Bob", bob);
+        peopleMap.put("Charlie", charlie);
+
+        for (Person p : peopleMap.values()) {
+            if (p.getAge() >= 30) {
+                System.out.println(p.getName());
+            }
+        }
+    }
+}
